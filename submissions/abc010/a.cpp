90c60b0d9e510c722cce94a4322e9a0f3fae452e
NinjaCipher <thienone1628.io@gmail.com>
2024-11-03 17:02:46 +0900

    add submission: 2024/11/03 17:02:46

---
+++ submissions/abc010/a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <map>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    string s;
+    cin >> s;
+    cout << s << "pp" << endl;
+    return 0;
+}
