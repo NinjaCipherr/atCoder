edcd179cee8fd548f94745eec417fee83239d4a0
NinjaCipher <thienone1628.io@gmail.com>
2024-11-03 17:24:27 +0900

    add submission: 2024/11/03 17:24:27

---
+++ submissions/abc004/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <map>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << n * 2 << endl;
+    return 0;
+}
---
+++ submissions/abc012/a.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <map>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    // string s;
+    cin >> a >> b;
+    int temp;
+    temp = b;
+    b = a;
+    a = temp;
+    cout << a << " " << b << endl;
+
+    return 0;
+}
